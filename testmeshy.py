3599f9bf06c10d17c40ca4a3e9aa34575efff715
wlstla24 <wlstla0124@gmail.com>
2025-04-03 10:35:48 -0700

    init

---
+++ testmeshy.py
@@ -0,0 +1,28 @@
+import requests
+
+YOUR_API_KEY = "msy_ZSlSbBAI5C3wXBHh3b9YFcStXGiUMhiBdl2d"
+
+
+
+payload = {
+  "mode": "preview",
+  "prompt": "a monster mask",
+  "art_style": "realistic",
+  "should_remesh": True,
+  "seed": 0,
+  "ai_model": "latest",
+  "topology": "triangle",
+  "target_polycount": 30000,
+  "symmetry_mode": "auto"
+}
+headers = {
+  "Authorization": f"Bearer {YOUR_API_KEY}"
+}
+
+response = requests.post(
+  "https://api.meshy.ai/openapi/v2/text-to-3d",
+  headers=headers,
+  json=payload,
+)
+response.raise_for_status()
+print(response.json())
